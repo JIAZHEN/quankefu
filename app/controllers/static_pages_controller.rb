88236d9aa381c078cc307b872b1764f880250321
Joe Xie <fdf515@gmail.com>
2017-02-02 23:25:12 +0000

    Action for baidu verify

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def baidu_verify
+    render :layout => false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#index"
 
-  get "/baidu_verify_kF7cKVAjVZ", :to => redirect("/baidu_verify_kF7cKVAjVZ.html")
+  get "/baidu_verify_kF7cKVAjVZ", :to => "static_pages#baidu_verify"
 end
