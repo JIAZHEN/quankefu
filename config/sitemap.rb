b470fb2cf271a54afeec1a5f06c01e66a93ad466
Joe Xie <fdf515@gmail.com>
2017-02-02 23:07:18 +0000

    Update default host

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://quankefu.net/"
+SitemapGenerator::Sitemap.default_host = "http://www.quankefu.net/"
 
 SitemapGenerator::Sitemap.public_path = "public/"
 
